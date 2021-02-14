27c98ab0f6f8371e5cee23b393b4d5a7d78f191f
Patrick Ware <patrick.r.ware@gmail.com>
2021-02-14 15:20:46 -0800

    removing unused code

--- src/App.js
+++ src/App.js
@@ -44,47 +44,6 @@ export default function App() {
     options: { radius: 75, maxZoom: 20 }
   });
 
-  // START MAP COMPONENT CODE
-
-
-//    // 1) Map setup
-//    const mapRef = useRef();
-//    const [bounds, setBounds] = useState(null);
-//    const [zoom, setZoom] = useState(10);
-    
-    // 2) load and format data
-    /* ORIG EARTHQUAKE CODE
-    const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-05-07&minmagnitude=3&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444"
-    const {data, error} = useSwr(url, fetcher)
-    const earthquakes = data && !error ? data.features :[];
-    console.log("HERE IS NEW DATA", earthquakes)
-
-    const points = earthquakes.map(quake => ({
-      "type": "Feature",
-      "properties": {
-        "cluster": false,
-        "quakeID": quake.id,
-        "magnitude": quake.properties.mag,
-        "place": quake.properties.place,
-      },
-      "geometry": { type: "Point", coordinates:[quake.geometry.coordinates[0], quake.geometry.coordinates[1]]
-      }
-    }))
-    console.log("and here are the points", points)
-
-    // 3) get clusters
-    const { clusters, supercluster } = useSupercluster({
-      points,
-      bounds,
-      zoom,
-      options: { radius: 75, maxZoom: 20 },
-    }); */
-
-  console.log("this is clusters", clusters);
-    // 4) render map
- 
-    // END MAP COMPONENT CODE
-
  return (
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
