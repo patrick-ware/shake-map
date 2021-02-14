ed5f2d74246b677882616a594d00691ba361b06d
Patrick Ware <patrick.r.ware@gmail.com>
2021-02-13 18:45:21 -0800

    3) get clusters updated to be {cluster,supercluster}

--- src/App.js
+++ src/App.js
@@ -151,37 +151,33 @@ function App() {
     const [bounds, setBounds] = useState(null);
     
     // 2) load and format data
-    const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-05-07&minmagnitude=5&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444"
+    const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-05-07&minmagnitude=4&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444"
     const {data, error} = useSwr(url, fetcher)
-    const earthquakes = data && !error ? data.features.slice(0,200) :[];
+    const earthquakes = data && !error ? data.features :[];
     console.log("HERE IS NEW DATA", earthquakes)
 
     const points = earthquakes.map(quake => ({
-      type: "Feature",
-      properties: {
-        cluster: false,
-        quakeID: quake.id,
-        magnitude: quake.properties.mag,
-        place: quake.properties.place,
+      "type": "Feature",
+      "properties": {
+        "cluster": false,
+        "quakeID": quake.id,
+        "magnitude": quake.properties.mag,
+        "place": quake.properties.place,
       },
-      geometry: { 
-        type: "Point", 
-        coordinates:[
-          quake.geometry.coordinates[0], 
-          quake.geometry.coordinates[1]
-        ]
+      "geometry": { type: "Point", coordinates:[quake.geometry.coordinates[0], quake.geometry.coordinates[1]]
       }
     }))
     console.log("and here are the points", points)    
 
     // 3) get clusters
-    const { clusters } = useSupercluster({
+    const { clusters, supercluster } = useSupercluster({
       points,
       bounds,
       zoom,
-      options: {radius: 75, maxZoom: 20}
-    })
-    console.log("this is clusters", clusters)
+      options: { radius: 75, maxZoom: 20 }
+    });
+
+    console.log("this is clusters", supercluster);
     // 4) render map
  
     // END MAP COMPONENT CODE
@@ -206,69 +202,68 @@ function App() {
         endDate={endDate}
         onEndChange={setEndDate}
       />
-    <div className="map">
-      <h2 className="map-h2" style={{color: "white"}}> TESTING</h2>
+      <div className="map">
+        <h2 className="map-h2" style={{color: "white"}}> TESTING</h2>
+        <div className="google-map">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API }}
+            defaultCenter={{ lat:38.1637, lng:-118.0837}}
+            defaultZoom={6}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map }) => {
+              mapRef.current = map;
+            }}
+            onChange={({zoom, bounds}) => {
+              setZoom(zoom);
+              setBounds([
+                bounds.nw.lng,
+                bounds.se.lat,
+                bounds.se.lnt,
+                bounds.nw.lat
+              ]);
+            }}
+          >
+          {clusters.map(cluster => {
+            const [longitude, latitude] = cluster.geometry.coordinates;
+            const {
+              cluster: isCluster,
+              point_count: pointCount
+            } = cluster.properties;
+            
+            if (isCluster) {
+            }
+            return (
+              <Marker
+                key={cluster.proerties.quakeID}
+                lat={latitude}
+                lng={longitude}
+              >
+                <div className="pin">
+                  <Icon icon={circleSlice8} className="pin-icon" style={{fontSize:"32pt"}}/>
+                </div>
+              </Marker>
+            )
+          })  }
 
-      <div className="google-map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API }}
-          defaultCenter={{ lat:38.1637, lng:-118.0837}}
-          defaultZoom={6}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map }) => {
-            mapRef.current = map;
-          }}
-          onChange={({zoom, bounds}) => {
-            setZoom(zoom);
-            setBounds([
-              bounds.nw.lng,
-              bounds.se.lat,
-              bounds.se.lnt,
-              bounds.nw.lat
-            ]);
-          }}
-        >
-        {clusters.map(cluster => {
-          const [longitude, latitude] = cluster.geometry.coordinates;
-          const {
-            cluster: isCluster,
-            point_count: pointCount
-          } = cluster.properties;
-          
-          if (isCluster) {
-          }
-          return (
-            <Marker
-              key={cluster.proerties.quakeID}
-              lat={latitude}
-              lng={longitude}
-            >
-              <div className="pin">
-                <Icon icon={circleSlice8} className="pin-icon" style={{fontSize:"32pt"}}/>
-              </div>
-            </Marker>
-          )
-        })  }
-
-          {
-            Object.entries(apiData).slice(0,200)
-              .map(([key, value]) => (
-                <Marker
-                  key={value.id}
-                  lat={value.geometry.coordinates[1]}
-                  lng={value.geometry.coordinates[0]}
-                >
-                  <div className="pin">
-                    <Icon icon={circleSlice8} className="pin-icon" style={{fontSize:"32pt"}}/>
-                    <p className="pin-text">{value.properties.place}</p>
-                  </div>
-                </Marker>
-            ))
-          }
-        </GoogleMapReact>
+            {/*{
+              Object.entries(apiData).slice(0,200)
+                .map(([key, value]) => (
+                  <Marker
+                    key={value.id}
+                    lat={value.geometry.coordinates[1]}
+                    lng={value.geometry.coordinates[0]}
+                  >
+                    <div className="pin">
+                      <Icon icon={circleSlice8} className="pin-icon" style={{fontSize:"32pt"}}/>
+                      <p className="pin-text">{value.properties.place}</p>
+                    </div>
+                  </Marker>
+              ))
+            }*/}
+          </GoogleMapReact>
+        </div>
       </div>
-    </div>
-      <BarChart 
+      {/*<BarChart 
         apiData={apiData}
         page={page}
         lastRecordDate={lastRecordDate}
@@ -276,7 +271,7 @@ function App() {
         goToPreviousPage={goToPreviousPage}
         goToNextPage={goToNextPage}
         setCoordinates={setCoordinates}
-      />
+      />*/}
     </div>
   );
 }
