481f9ea943dab507e77964a52c7fcbc2b6700f1b
Patrick Ware <patrick.r.ware@gmail.com>
2021-03-02 22:42:29 -0800

    rearrange input and label

--- src/components/MagInput/MagInput.js
+++ src/components/MagInput/MagInput.js
@@ -4,17 +4,17 @@ import './MagInput.css';
 function MagInput(props) {
   return (
     <div style={{display: "inline-block"}}>
+      <input 
+        className="MagInput" 
+        type="number" 
+        id="minmag" 
+        min="1.0" 
+        max="10.0"
+        step="0.1"
+      value={props.mag} 
+      onChange={props.changeMag} 
+      />
       <label htmlFor="minmag"> {props.label}:</label>
-        <input 
-          className="MagInput" 
-          type="number" 
-          id="minmag" 
-          min="1.0" 
-          max="10.0"
-          step="0.1"
-        value={props.mag} 
-        onChange={props.changeMag} 
-        />
     </div>
   )
 }
